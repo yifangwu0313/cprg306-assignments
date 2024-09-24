9ddee4eb384ef68aaeb6079127d64eab5685d57a
yifangwu0313 <165215061+yifangwu0313@users.noreply.github.com>
2024-09-24 02:15:02 -0600

    add week-3

---
+++ app/week-3/item.js
@@ -0,0 +1,12 @@
+export default function Item({ name, quantity, category }) {
+  return (
+    <ul className="bg-yellow-500 m-2 w-[30%]  pl-3 py-2 rounded">
+      <li>
+        <h1 className="text-xl font-bold">{name}</h1>
+        <p>
+          Buy {quantity} in {category}
+        </p>
+      </li>
+    </ul>
+  );
+}
