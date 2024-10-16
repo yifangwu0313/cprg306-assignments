fa2f111cbbedf4c57956364b6cb9397c8b3ce3ba
yifangwu0313 <165215061+yifangwu0313@users.noreply.github.com>
2024-10-16 00:33:35 -0600

    add week-6

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -1,16 +1,39 @@
-import Item from "./item";
+"use client";
+import { useState } from "react";
+import Item from "./item.js";
+import items from "./items.json";
 
-export default function ItemList({ items }) {
-  return (
-    <div className="flex flex-col gap-3 my-5 ml-5">
-      {items.map((item) => (
-        <Item
-        key={item.ItemName} 
-        name={item.ItemName} 
-        quantity={item.Quantity} 
-        category={item.Category} 
-        />
-      ))}
-    </div>
-  );
+export default function ItemList(){
+
+    let itemJSON = [...items]; 
+    const [sortBy, setSortBy] = useState("name");
+    itemJSON.sort((a, b) => {
+        if (sortBy === "name") {
+            return a.name.localeCompare(b.name);
+        }
+        else if (sortBy === "category") {
+            return a.category.localeCompare(b.category);
+        }
+        return 0;
+    });
+
+    return(
+        <main>
+            <div 
+            className="flex">
+            <h1>Sort by:</h1>
+            <button
+            onClick={() => setSortBy("name")}
+            className={`py-2 px-4 bg-blue-400 border-b-1 font-bold ml-2 hover:bg-blue-900 ${sortBy === "name" ? "bg-blue-950 text-gray-500" : "bg-gray-300 text-white"}`}>Name</button>
+            <button
+            onClick={()=> setSortBy("category")}
+            className={`py-2 px-4 bg-blue-400 border-b-1 font-bold ml-4 hover:bg-blue-900 ${sortBy === "category" ? "bg-blue-950 text-gray-500" : "bg-gray-300 text-white"}`}>Category</button>
+       </div> 
+       <div>
+              {itemJSON.map((item, index) => {
+                return <Item key={index} name={item.name} quantity={item.quantity} category={item.category} />
+              })}
+       </div>
+       </main>
+    )
 }
